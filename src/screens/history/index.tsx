3ce66d6a888d3a71ea4948bf15c8eb5c6809b0b6
Ross <528266131@qq.com>
2022-06-06 22:50:12 +0800

    add history screen

---
+++ src/screens/history/index.tsx
@@ -0,0 +1,130 @@
+import { IHistory } from '@/interfaces/history';
+import { useAppDispatch, useAppSelector } from '@/utils/hooks';
+import dayjs from 'dayjs';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import {
+  TouchableOpacity,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+} from 'react-native';
+import Record from './components/record';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import { Colors } from '@/theme/colors';
+import { useNavigation } from '@react-navigation/core';
+import { historyActions } from '@/store/reducers/history';
+import Common from '@/theme/common';
+import updateLocale from 'dayjs/plugin/updateLocale';
+dayjs.extend(updateLocale);
+
+dayjs.extend(localizedFormat);
+dayjs.updateLocale('zh-cn', {
+  formats: {
+    L: 'M月D日',
+    LL: 'YYYY年M月D日',
+  },
+});
+
+interface IGroupMap {
+  [date: string]: Array<IHistory>;
+}
+
+interface IGroupSection {
+  title: string;
+  data: Array<IHistory>;
+}
+
+const History = () => {
+  const navigation = useNavigation();
+  const dispatch = useAppDispatch();
+  const list = useAppSelector((state) => state.history.list);
+  const [groups, setGroups] = useState<Array<IGroupSection>>([]);
+
+  useLayoutEffect(() => {
+    let headerRight = null;
+    if (list.length > 0) {
+      headerRight = () => (
+        <TouchableOpacity
+          style={Common.headerRight}
+          onPress={() => {
+            Alert.alert('提示', '确认清空历史记录吗？', [
+              {
+                text: '确认',
+                onPress: () => dispatch(historyActions.clear({})),
+                style: 'default',
+              },
+              {
+                text: '取消',
+                style: 'cancel',
+              },
+            ]);
+          }}>
+          <Text style={Common.headerRightText}>清空</Text>
+        </TouchableOpacity>
+      );
+    }
+
+    navigation.setOptions({
+      headerRight: headerRight,
+    });
+  }, [list, dispatch, navigation]);
+
+  useEffect(() => {
+    const group: IGroupMap = {};
+    const today = dayjs().format('YYYY-MM-DD');
+
+    list.forEach((item) => {
+      const date = dayjs(item.recordedAt).format('YYYY-MM-DD');
+
+      if (!group[date]) {
+        group[date] = [];
+      }
+
+      group[date].push(item);
+    });
+
+    const _groups = Object.keys(group).map((key) => {
+      const date = today === key ? '今天' : dayjs(key).format('L');
+
+      return {
+        title: date,
+        data: group[key],
+      };
+    });
+    setGroups(_groups);
+  }, [list]);
+
+  return (
+    <View style={styles.container}>
+      <SectionList
+        sections={groups}
+        renderItem={({ item }) => <Record item={item} />}
+        keyExtractor={(item) => `history_${item.id}`}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionTitle}>{section.title}</Text>
+        )}
+        contentContainerStyle={styles.sectionContainer}
+      />
+    </View>
+  );
+};
+
+export default History;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.lightGrey,
+    flex: 1,
+  },
+  sectionContainer: {
+    paddingHorizontal: 12,
+    paddingBottom: 16,
+  },
+  sectionTitle: {
+    marginTop: 16,
+    marginBottom: 4,
+    fontSize: 16,
+  },
+});
